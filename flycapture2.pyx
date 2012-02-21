05d849183afc4bf8398d77152b44d7513778a8be
jordens@phys.ethz.ch <>
2012-02-21 00:08:02 -0700

    add setup

--- flycapture2.pyx
+++ flycapture2.pyx
@@ -1,3 +1,11 @@
-cimport _FlyCapture2_C_merged
+from _FlyCapture2_C_merged cimport *
 
+cdef class FlyCapture2:
+    cdef fc2Context *_ctx
+    def __cinit__(self):
+        self.check_error(fc2CreateContext(self._ctx))
 
+    def get_num_of_cameras(self):
+        cdef unsigned int n
+        self.check_error(fc2GetNumOfCameras(self._ctx, &n))
+        return n
