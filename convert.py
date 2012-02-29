78b17b8ce1271642e9014070abf302e1a9ea94cf
jordens@phys.ethz.ch <>
2012-02-29 00:22:19 -0700

    add windows dirs, libnames, numpy includes

--- convert.py
+++ convert.py
@@ -1,8 +1,26 @@
+# -*- coding: utf8 -*-
+#
+#   pyflycapture2 - python bindings for libflycapture2_c
+#   Copyright (C) 2012 Robert Jordens <jordens@phys.ethz.ch>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from cwrap.config import Config, File
 
 if __name__ == '__main__':
     config = Config('gccxml', files=[
-	File('FlyCapture2Defs_C.h'),
-	File('FlyCapture2_C.h'),
-	])
+        File('FlyCapture2Defs_C.h'),
+        File('FlyCapture2_C.h'),
+    ])
     config.generate()
--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+#
+#   pyflycapture2 - python bindings for libflycapture2_c
+#   Copyright (C) 2012 Robert Jordens <jordens@phys.ethz.ch>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 try:
     from setuptools import setup, Extension
 except ImportError:
@@ -5,6 +24,14 @@ except ImportError:
     from distutils.extension import Extension
 
 from Cython.Distutils import build_ext
+import numpy
+import os
+
+if os.name == "posix":
+    libname = "flycapture-c"
+else:
+    libname = "flycapture2_c"
+pointgrey_win = "C:/Program Files (x86)/Point Grey Research/FlyCapture2"
 
 setup(
     name="pyflycapture2",
@@ -17,8 +44,14 @@ setup(
     install_requires=["numpy"],
     #packages=["flycapture2"],
     cmdclass = {'build_ext': build_ext},
+    #"test_flycapture2.py", "convert.py"
     ext_modules = [Extension("flycapture2",
-        sources = ["src/flycapture2.pyx"],
-        libraries = ["flycapture-c"],
-        include_dirs = ["/usr/include/flycapture/C"])]
+        sources = ["src/flycapture2.pyx", "src/flycapture2_enums.pxi",
+            "src/_FlyCapture2Defs_C.pxd", "src/_FlyCapture2_C.pxd",],
+        libraries = [libname],
+        library_dirs = ["%s/lib/C" % pointgrey_win],
+        include_dirs = ["/usr/include/flycapture/C",
+            "%s/include/C" % pointgrey_win,
+            numpy.get_include(), ],
+        ),]
 )
